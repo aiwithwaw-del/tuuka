6590b6cdaf4452c09703bf3b3c8a116c51c48598
aiwithwaw-del <a.i.withwaw@gmail.com>
2026-07-08 11:48:13 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class TuukaApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Tuuka',
+      title: 'Fuuna',
       debugShowCheckedModeBanner: false, // Faster
       theme: ThemeData(
         primarySwatch: Colors.blue,
